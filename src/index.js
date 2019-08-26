0e1884e40b11c103c98e471f1dcddeac47158c9d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-26 15:39:14 -0400

    Alters redux store definition to an import

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 
 // Redux
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { Router } from 'react-router-dom';
 import history from './history.js';
-import rootReducer from 'reducers/';
+import store from './store.js';
 
 // Axios interceptors
 import './interceptors.js';
@@ -16,12 +14,6 @@ import './interceptors.js';
 
 import App from './App';
 import 'SCSS/';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
 
 ReactDOM.render(
   <Provider store={store}>
