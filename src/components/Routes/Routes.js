2d6bd59c4d780a5d2c6ea7ec102c6bae01b9ac23
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-01 23:12:46 -0400

    Adds route to start game

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import GameSelect from 'components/protected/GameSelect/';
 import NewGame from 'components/protected/GameSelect/NewGame/';
 import JoinGame from 'components/protected/GameSelect/JoinGame/';
+import PlayGame from 'components/protected/Game/';
 
 function Routes(props) {
   return (
@@ -11,6 +12,7 @@ function Routes(props) {
       <Route path="/" exact render={rProps => <GameSelect {...rProps} />} />
       <Route path="/game/create" component={NewGame} />
       <Route path="/game/join" component={JoinGame} />
+      <Route path="/game/play/:name" component={PlayGame} />
     </React.Fragment>
   );
 }
