b8d96ef786b8a05e624c7885394d4117d612fe93
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-08 03:36:30 -0400

    Adds getGame API call on load

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import GameSelect from 'components/protected/GameSelect/';
 import NewGame from 'components/protected/GameSelect/NewGame/';
 import JoinGame from 'components/protected/GameSelect/JoinGame/';
 import PlayGame from 'components/protected/Game/';
 
+import { getUsersGames } from 'reducers/account';
+
 function Routes(props) {
+  console.log(props.games);
+  const { gamesWereFetched, games, getUsersGames } = props;
+  useEffect(() => {
+    if (!gamesWereFetched && !games.length) {
+      getUsersGames();
+    }
+  }, [gamesWereFetched, games, getUsersGames]);
   return (
     <React.Fragment>
       <Route path="/" exact render={rProps => <GameSelect {...rProps} />} />
       <Route path="/game/create" component={NewGame} />
       <Route path="/game/join" component={JoinGame} />
-      <Route path="/game/play/:name" component={PlayGame} />
+      <Route
+        path="/game/play/:name"
+        render={rProps => (
+          <PlayGame
+            {...rProps}
+            game={props.games.find(g => g.name === rProps.match.params.name)}
+          />
+        )}
+      />
     </React.Fragment>
   );
 }
 
-export default Routes;
+const mapStateToProps = state => ({
+  games: state.account.games,
+  gamesWereFetched: state.account.gamesWereFetched
+});
+
+export default connect(
+  mapStateToProps,
+  { getUsersGames }
+)(Routes);
