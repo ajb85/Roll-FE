c991708352e5cdd4663bdbf214e0ca80dafdc707
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-10-19 23:57:38 -0400

    Abstracts everything sockets

---
+++ src/sockets/index.js
@@ -0,0 +1,43 @@
+import io from 'socket.io-client';
+import listeners from './listeners';
+
+class SocketsManager {
+  constructor() {
+    this.io = io;
+    this.socket = this.io.connect(process.env.REACT_APP_API_URL);
+    this.listeners = listeners;
+
+    this.errorHandling();
+  }
+
+  errorHandling() {
+    return this.socket.on('error', function(err) {
+      console.log('received socket error:');
+      console.log(err);
+    });
+  }
+
+  subscribeToGames(games) {
+    return this.emit('joinGames', games);
+  }
+
+  emit(room, data) {
+    return this.socket.emit(room, data);
+  }
+
+  listen(room, cb) {
+    return this.socket.on(room, cb);
+  }
+
+  join(room) {
+    this.listen(room, (context, message) => {
+      this.listeners.game[context](message);
+    });
+  }
+
+  joinList(games) {
+    games.forEach(room => this.join(room));
+  }
+}
+
+export default new SocketsManager();
