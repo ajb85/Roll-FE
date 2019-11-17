42e392551f4f8d777c21341bfabde53277a2e989
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-17 17:42:00 -0500

    Updates game to use new packages queries

--- src/components/protected/Game/ScoreTable/ScoreTable.js
+++ src/components/protected/Game/ScoreTable/ScoreTable.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import predictScore from 'js/predictScore.js';
 import styles from '../styles.module.scss';
 
-function ScoreTable({ game, selected, setSelected }) {
+function ScoreTable({ game, selected, setSelected, user_id }) {
   const { scores, rolls } = game;
+  const rawUserScore = scores.find(s => s.user_id === user_id);
   const dice = rolls && rolls.length ? rolls[rolls.length - 1] : [];
   const userScore = selected
-    ? predictScore(selected, dice, scores.user)
-    : scores.user || {};
+    ? predictScore(selected, dice, rawUserScore)
+    : rawUserScore || {};
   const toggleSelected = e => {
     setSelected(e.target.id === selected ? null : e.target.id);
   };
@@ -40,9 +42,15 @@ function ScoreTable({ game, selected, setSelected }) {
       {leftCategories.map((l, i) => {
         const r = rightCategories[i];
         const lClickable =
-          l.isClickable && game.rolls.length && scores.user[l.name] === null;
+          l.isClickable &&
+          game.rolls &&
+          game.rolls.length &&
+          rawUserScore[l.name] === null;
         const rClickable =
-          r.isClickable && game.rolls.length && scores.user[r.name] === null;
+          r.isClickable &&
+          game.rolls &&
+          game.rolls.length &&
+          rawUserScore[r.name] === null;
         return (
           <tr key={`${l.name} ${r.name}`}>
             <td>{l.name}</td>
@@ -54,7 +62,7 @@ function ScoreTable({ game, selected, setSelected }) {
                 lClickable
                   ? selected === l.name
                     ? styles.selected
-                    : scores.user[l.name]
+                    : rawUserScore[l.name]
                     ? null
                     : styles.scoreCell
                   : null
@@ -62,7 +70,7 @@ function ScoreTable({ game, selected, setSelected }) {
             >
               {userScore[l.name]}
             </td>
-            <td>{scores.leader[l.name]}</td>
+            <td>{scores[0][l.name]}</td>
             <td>{r.name}</td>
             <td
               id={r.name}
@@ -71,7 +79,7 @@ function ScoreTable({ game, selected, setSelected }) {
                 rClickable
                   ? selected === r.name
                     ? styles.selected
-                    : scores.user[r.name]
+                    : rawUserScore[r.name]
                     ? null
                     : styles.scoreCell
                   : null
@@ -80,24 +88,28 @@ function ScoreTable({ game, selected, setSelected }) {
               {userScore[r.name]}
             </td>
 
-            <td>{scores.leader[r.name]}</td>
+            <td>{scores[0][r.name]}</td>
           </tr>
         );
       })}
 
       <tr style={{ fontWeight: 800 }}>
-        <td style={{ padding: '5px', fontSize: '0.8rem' }} colSpan="4">
+        <td style={{ padding: '5px', fontSize: '0.8rem' }} colSpan='4'>
           Grand Total
         </td>
         <td style={{ padding: '5px', fontSize: '0.8rem' }}>
           {userScore['Grand Total']}
         </td>
         <td style={{ padding: '5px', fontSize: '0.8rem' }}>
-          {scores.leader['Grand Total']}
+          {scores[0]['Grand Total']}
         </td>
       </tr>
     </tbody>
   );
 }
 
-export default ScoreTable;
+const mapStateToProps = state => ({
+  user_id: state.account.id
+});
+
+export default connect(mapStateToProps, {})(ScoreTable);
