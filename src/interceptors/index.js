a17a904b4a39042735cf01fe117b359337c039fc
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-24 18:55:28 -0500

    Adds query to get user info if there is no saved user info

--- src/interceptors/index.js
+++ src/interceptors/index.js
@@ -3,6 +3,7 @@ import store from '../store.js';
 import Sockets from 'sockets/';
 import { success, failure } from './responses.js';
 import { loading } from '../reducers/app.js';
+import { populateAccount } from '../reducers/account.js';
 
 axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}/api`;
 
@@ -10,9 +11,15 @@ axios.interceptors.request.use(req => {
   store.dispatch(loading());
   const state = store.getState();
   const token = state.account.token;
+
   if (token) {
     Sockets.emit('identify', token);
     req.headers.authorization = token;
+  }
+
+  if (!state.account.id && !(req.url === '/auth' && req.method === 'get')) {
+    // Get account info if not found in state
+    store.dispatch(populateAccount());
   }
   return req;
 });
