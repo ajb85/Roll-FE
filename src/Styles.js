93dcf0b754e423a7cf47aca599bcd1900837a265
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-28 02:36:09 -0500

    Adds styled components to handle dynamic colors

---
+++ src/Styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { colors } from 'js/Colors.js';
+
+export const AppContainer = styled.div`
+  background-color: ${props => props.colors.primary};
+  height: 100vh;
+  transition: 0.5s;
+
+  p,
+  span,
+  input,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  div {
+    transition: 0.5s;
+    color: ${props => props.colors.secondary};
+  }
+
+  button {
+    background-color: ${props => props.colors.primary};
+    color: ${props => props.colors.secondary};
+    border: 1px solid ${props => props.colors.secondary};
+
+    &:hover {
+      background-color: ${props => props.colors.secondary};
+      color: ${props => props.colors.primary};
+    }
+  }
+`;
