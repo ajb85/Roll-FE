db5b015e1df79c709dd63c65e1c75f00c53813da
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-28 02:33:34 -0500

    Integrates color context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
@@ -7,18 +7,27 @@ import LoggedInRoutes from './components/Routes/';
 import { setToken } from 'reducers/account.js';
 
 import styles from './styles.module.scss';
+import { AppContainer } from 'Styles.js';
+
+import { colorContext } from 'js/Colors.js';
 
 function App(props) {
+  const { colors } = useContext(colorContext);
+  console.log('COLOR: ', colors);
   return (
-    <div className={styles.App}>
-      {props.showHeader && <h1>Roll!</h1>}
-      <Route path='/' component={props.token ? LoggedInRoutes : Account} />
-      {props.token && props.showHeader && (
-        <button className={styles.logout} onClick={() => props.setToken()}>
-          Logout
-        </button>
-      )}
-    </div>
+    <AppContainer
+      colors={colors || { primary: '', secondary: '', highlight: '' }}
+    >
+      <div className={styles.App}>
+        {props.showHeader && <h1>Roll!</h1>}
+        <Route path='/' component={props.token ? LoggedInRoutes : Account} />
+        {props.token && props.showHeader && (
+          <button className={styles.logout} onClick={() => props.setToken()}>
+            Logout
+          </button>
+        )}
+      </div>
+    </AppContainer>
   );
 }
 
