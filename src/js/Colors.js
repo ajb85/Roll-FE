7ce30d802c2e80a15112fbbcb7402cb796961650
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-28 02:33:43 -0500

    Adds color context

---
+++ src/js/Colors.js
@@ -0,0 +1,58 @@
+import React, { useState, createContext } from 'react';
+export const colorContext = createContext();
+
+class ColorManager {
+  constructor() {
+    this.dark = {
+      primary: '#191919',
+      secondary: '#949494',
+      highlight: '#632B30',
+      mode: 'dark'
+    };
+
+    this.light = {
+      primary: '#FCFCFC',
+      secondary: '#191919',
+      highlight: '#632B30',
+      mode: 'light'
+    };
+
+    this.set = 'dark';
+    // this.primary;
+    // this.secondary;
+    // this.highlight;
+    // this.set;
+  }
+
+  setTo(mode) {
+    if (!mode) {
+      const convert = { dark: 'light', light: 'dark' };
+      mode = convert[this.set];
+    }
+    this.set = mode;
+
+    // for (let color in this[mode]) {
+    //   this[color] = this[mode][color];
+    // }
+    // this.set = mode;
+    return this[mode];
+  }
+}
+export const colors = new ColorManager();
+
+const ColorProvider = props => {
+  const [current, setCurrent] = useState(colors.dark);
+  const { Provider } = colorContext;
+  return (
+    <Provider
+      value={{
+        colors: current,
+        switchMode: mode => setCurrent(colors.setTo(mode))
+      }}
+    >
+      {props.children}
+    </Provider>
+  );
+};
+
+export default ColorProvider;
