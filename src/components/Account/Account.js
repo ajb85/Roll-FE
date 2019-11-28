e394da01c40d9b5034b3549b006fad9ecc3d9671
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-28 02:35:21 -0500

    Adds light/dark mode and additional styles

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { connect } from 'react-redux';
 
 import { authAccount } from 'reducers/account.js';
 import styles from './styles.module.scss';
 
+import { colorContext } from 'js/Colors.js';
+
 function Account(props) {
   const [isRegistering, setIsRegistering] = useState(false);
   const { authAccount, isLoading } = props;
   const [form, setForm] = useState({ username: '', password: '', email: '' });
+  const { colors, switchMode } = useContext(colorContext);
   const setState = e => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleClick = e => {
+    const condition = {
+      register: !isRegistering,
+      login: isRegistering
+    };
+    if (condition[e.target.name]) {
+      setIsRegistering(!isRegistering);
+    }
+  };
 
   const onSubmit = e => {
     e.preventDefault();
-    const { username, password, email } = form;
-    authAccount(
-      isRegistering
-        ? { username, password, email }
-        : { account: username, password }
-    );
+
+    // const { username, password, email } = form;
+    // authAccount(
+    //   isRegistering
+    //     ? { username, password, email }
+    //     : { account: username, password }
+    // );
   };
 
   return (
@@ -48,7 +62,7 @@ function Account(props) {
             placeholder='Email'
             style={{ display: isRegistering ? 'initial' : 'none' }}
           />
-          {/* )} */}
+
           <input
             name='password'
             type='password'
@@ -63,20 +77,56 @@ function Account(props) {
         </p>
 
         <div className={styles.buttons}>
-          <button type='submit'>{isRegistering ? 'Register' : 'Login'}</button>
-          <p>OR</p>
           <button
-            type='button'
-            onClick={() => setIsRegistering(!isRegistering)}
+            type={isRegistering ? 'button' : 'submit'}
+            name='login'
+            style={{
+              backgroundColor: isRegistering
+                ? colors.primary
+                : colors.secondary,
+              color: isRegistering ? colors.secondary : colors.primary
+            }}
+            onClick={e => handleClick(e)}
           >
-            {isLoading
-              ? '...Loading'
-              : isRegistering
-              ? 'Login to Account'
-              : 'Create Account'}
+            {isLoading ? '...Loading' : 'Login'}
+          </button>
+          <div
+            className={styles.bubble}
+            style={{
+              width: '25px'
+            }}
+          >
+            <div style={{ backgroundColor: colors.primary }} />
+            <div style={{ backgroundColor: colors.primary }} />
+            <p
+              style={{
+                color: colors.secondary,
+                backgroundColor: colors.primary,
+                border: `1px solid ${colors.secondary}`
+              }}
+            >
+              OR
+            </p>
+          </div>
+          <button
+            type={isRegistering ? 'submit' : 'button'}
+            name='register'
+            style={{
+              backgroundColor: isRegistering
+                ? colors.secondary
+                : colors.primary,
+              color: isRegistering ? colors.primary : colors.secondary
+            }}
+            onClick={e => handleClick(e)}
+          >
+            {isLoading ? '...Loading' : 'Register'}
           </button>
         </div>
       </form>
+
+      <div className={styles.darkMode} onClick={() => switchMode()}>
+        <p>Switch to {colors.mode === 'dark' ? 'light' : 'dark'} mode.</p>
+      </div>
     </section>
   );
 }
