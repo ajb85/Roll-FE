444805189518a22500f2ea9934bd283c450cd054
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-12-01 16:19:32 -0500

    Minor tweaks to resolve 'left' issue with slider

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -11,7 +11,7 @@ import styles from './styles.module.scss';
 import { colorContext } from 'js/Colors.js';
 
 function Account(props) {
-  const [isRegistering, setIsRegistering] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(null);
   const { authAccount } = props;
   const formState = useState({ username: '', password: '', email: '' });
   const { colors, switchMode } = useContext(colorContext);
@@ -19,9 +19,12 @@ function Account(props) {
   return (
     <section className={styles.Account}>
       <div className={styles.container}>
-        <div
+        <Slider
+          isRegistering={isRegistering}
           className={styles.slider}
-          style={{ left: isRegistering ? '0px' : '492px' }}
+          // style={{
+          //   left: isRegistering ? '0px' : '492px'
+          // }}
         >
           <Login
             state={formState}
@@ -35,7 +38,7 @@ function Account(props) {
             colors={colors}
             setIsRegistering={setIsRegistering}
           />
-        </div>
+        </Slider>
       </div>
       <div className={styles.darkMode} onClick={() => switchMode()}>
         <p>Switch to {colors.mode === 'dark' ? 'light' : 'dark'} mode.</p>
--- src/components/Account/Styles.js
+++ src/components/Account/Styles.js
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
 export const Slider = styled.div`
-  left: ${({ isRegistering }) => {
-    console.log('IS REGISTERING: ', isRegistering ? '0px' : '492px');
-    return isRegistering ? '0px' : '492px';
-  }};
+  left: ${({ isRegistering }) => (isRegistering ? '0px' : '492px')};
 `;
