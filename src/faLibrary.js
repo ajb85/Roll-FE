d917331552c3a355b26a36a497630cdd8cdcc055
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-14 18:14:14 -0500

    Creates library to import FA icons

---
+++ src/faLibrary.js
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faLightbulbSlash,
+  faLightbulbOn
+} from '@fortawesome/pro-solid-svg-icons';
+
+library.add(faLightbulbSlash, faLightbulbOn);
