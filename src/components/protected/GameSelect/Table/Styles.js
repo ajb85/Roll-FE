90d8ec7d7785cedf5814695c24a150e76f6a4c4d
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-21 21:07:06 -0500

    Controls color of highlight

---
+++ src/components/protected/GameSelect/Table/Styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Row = styled.tr`
+  background-color: ${props =>
+    props.isUsersTurn ? props.colors.secondary : props.colors.primary};
+  color: ${props =>
+    props.isUsersTurn ? props.colors.primary : props.colors.secondary};
+
+  border-radius: ${props => (props.isUsersTurn ? '7px' : 0)};
+`;
