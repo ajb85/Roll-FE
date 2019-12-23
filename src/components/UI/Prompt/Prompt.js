958521703bd44daeb3ad6a3fc61709f3f40ef6d0
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-23 04:23:08 -0500

    Alters shadow mask to use dynamic color

--- src/components/UI/Prompt/Prompt.js
+++ src/components/UI/Prompt/Prompt.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+import { colorContext } from 'js/Colors.js';
+import { Shadow } from './Styles.js';
 import styles from './styles.module.scss';
 
 function Prompt(props) {
   const { action, cancel, confirmCopy, cancelCopy } = props;
-
+  const { colors } = React.useContext(colorContext);
   return (
     <>
-      <div onClick={cancel} className={styles.shadow} />
-      <div className={styles.content}>
-        <p>{props.children}</p>
+      <Shadow colors={colors} onClick={cancel} className={styles.shadow} />
+      <div
+        className={styles.content}
+        style={{ backgroundColor: colors.secondary }}
+      >
+        <p style={{ color: colors.primary }}>{props.children}</p>
         <div>
           <button onClick={action}>{confirmCopy || 'Yes'}</button>
           <button onClick={cancel}>{cancelCopy || 'Cancel'}</button>
