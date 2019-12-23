bbca49a6ef9f0a5715399a71c7249ffaa8cd2ada
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-23 03:28:57 -0500

    Now considers if game is inactive

--- src/components/protected/GameSelect/Table/Styles.js
+++ src/components/protected/GameSelect/Table/Styles.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 export const Row = styled.tr`
   background-color: ${props =>
-    props.isUsersTurn ? props.colors.secondary : props.colors.primary};
+    props.isUsersTurn && props.isActive
+      ? props.colors.secondary
+      : props.colors.primary};
   color: ${props =>
-    props.isUsersTurn ? props.colors.primary : props.colors.secondary};
+    props.isUsersTurn && props.isActive
+      ? props.colors.primary
+      : props.colors.secondary};
 
   border-radius: ${props => (props.isUsersTurn ? '7px' : 0)};
 `;
