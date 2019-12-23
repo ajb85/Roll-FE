0c000371d862ac360cab8e90be43aa1cd18fb845
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-23 01:57:33 -0500

    Moves ugly logic to its own file

---
+++ src/js/categories.js
@@ -0,0 +1,21 @@
+export const leftCategories = [
+  { name: 'Ones', isClickable: true },
+  { name: 'Twos', isClickable: true },
+  { name: 'Threes', isClickable: true },
+  { name: 'Fours', isClickable: true },
+  { name: 'Fives', isClickable: true },
+  { name: 'Sixes', isClickable: true },
+  { name: 'Left Bonus', isClickable: false },
+  { name: 'Left Total', isClickable: false }
+];
+
+export const rightCategories = [
+  { name: '3 of a Kind', isClickable: true },
+  { name: '4 of a Kind', isClickable: true },
+  { name: 'Full House', isClickable: true },
+  { name: 'Sm Straight', isClickable: true },
+  { name: 'Lg Straight', isClickable: true },
+  { name: 'Roll!', isClickable: true },
+  { name: 'Roll! Bonus', isClickable: false },
+  { name: 'Free Space', isClickable: true }
+];
