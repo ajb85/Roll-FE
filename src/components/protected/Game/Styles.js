9dd280f92977b0f3d9bf175e81cc81e38e57e6aa
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-24 16:21:36 -0500

    Updates style of the game table to match the game list

--- src/components/protected/Game/Styles.js
+++ src/components/protected/Game/Styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
-export const Table = styled.table`
+export const Table = styled.div`
   border-color: ${({ colors }) => colors.secondary};
 
-  th,
-  td {
+  th {
     border-color: ${({ colors }) => colors.secondary};
+  }
+
+  td {
+    border-color: ${({ colors }) => colors.primary};
   }
 `;
