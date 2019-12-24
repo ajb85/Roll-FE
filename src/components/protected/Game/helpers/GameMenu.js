2e514b51ae80b6d5294b5dfb32e3d0968d54a728
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-24 17:03:16 -0500

    Adds additional icons for game menus

--- src/components/protected/Game/helpers/GameMenu.js
+++ src/components/protected/Game/helpers/GameMenu.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import history from 'history.js';
+import LightMode from 'components/UI/LightMode';
 import { getGameRound } from 'js/rounds';
 import { colorContext } from 'js/Colors.js';
 
@@ -10,13 +12,22 @@ function GameMenu({ game }) {
   const { colors } = React.useContext(colorContext);
   return (
     <>
-      <p
-        style={{ borderColor: colors.secondary }}
-        onClick={() => history.push('/')}
-        className={styles.toLobby}
-      >
-        {'<'} Return to Lobby
-      </p>
+      <div className={styles.controls}>
+        <FontAwesomeIcon
+          icon='chevron-square-left'
+          style={{ borderColor: colors.secondary }}
+          onClick={() => history.push('/')}
+        />
+        <LightMode inline={true} />
+        <FontAwesomeIcon
+          icon={game.isJoinable ? 'lock-open-alt' : 'lock-alt'}
+          style={{ borderColor: colors.secondary }}
+        />
+        <FontAwesomeIcon
+          icon='user-plus'
+          style={{ borderColor: colors.secondary }}
+        />
+      </div>
       <table className={styles.menu}>
         <thead>
           <tr>
--- src/faLibrary.js
+++ src/faLibrary.js
@@ -1,7 +1,18 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faLightbulbSlash,
-  faLightbulbOn
+  faLightbulbOn,
+  faChevronSquareLeft,
+  faUserPlus,
+  faLockOpenAlt,
+  faLockAlt
 } from '@fortawesome/pro-solid-svg-icons';
 
-library.add(faLightbulbSlash, faLightbulbOn);
+library.add(
+  faLightbulbSlash,
+  faLightbulbOn,
+  faChevronSquareLeft,
+  faUserPlus,
+  faLockOpenAlt,
+  faLockAlt
+);
