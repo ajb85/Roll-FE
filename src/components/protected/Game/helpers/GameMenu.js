645f8551e4642cdb4e72b2f7691b944dd1c8a49b
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-24 19:25:55 -0500

    Adds menu items

--- src/components/protected/Game/helpers/GameMenu.js
+++ src/components/protected/Game/helpers/GameMenu.js
@@ -4,29 +4,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import history from 'history.js';
 import LightMode from 'components/UI/LightMode';
 import { getGameRound } from 'js/rounds';
-import { colorContext } from 'js/Colors.js';
+// import { colorContext } from 'js/Colors.js';
 
 import styles from '../styles.module.scss';
 
-function GameMenu({ game }) {
-  const { colors } = React.useContext(colorContext);
+function GameMenu({ game, togglePrompt, isOwner }) {
+  // const { colors } = React.useContext(colorContext);
   return (
     <>
       <div className={styles.controls}>
         <FontAwesomeIcon
           icon='chevron-square-left'
-          style={{ borderColor: colors.secondary }}
           onClick={() => history.push('/')}
         />
         <LightMode inline={true} />
         <FontAwesomeIcon
           icon={game.isJoinable ? 'lock-open-alt' : 'lock-alt'}
-          style={{ borderColor: colors.secondary }}
+          style={{ opacity: isOwner ? 1 : 0.5 }}
         />
-        <FontAwesomeIcon
-          icon='user-plus'
-          style={{ borderColor: colors.secondary }}
-        />
+        {isOwner && <FontAwesomeIcon icon='user-plus' onClick={togglePrompt} />}
       </div>
       <table className={styles.menu}>
         <thead>
