2ef0c91fcd47880889b563d91b28e2c929b13a3a
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-24 19:25:36 -0500

    Modifies Prompt to allow it to copy

--- src/components/UI/Prompt/Prompt.js
+++ src/components/UI/Prompt/Prompt.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import { colorContext } from 'js/Colors.js';
 import { Shadow } from './Styles.js';
 import styles from './styles.module.scss';
 
 function Prompt(props) {
-  const { action, cancel, confirmCopy, cancelCopy } = props;
+  const { action, cancel, confirmCopy, cancelCopy, copy } = props;
   const { colors } = React.useContext(colorContext);
   return (
     <>
@@ -16,7 +17,13 @@ function Prompt(props) {
       >
         <p style={{ color: colors.primary }}>{props.children}</p>
         <div>
-          <button onClick={action}>{confirmCopy || 'Yes'}</button>
+          {copy ? (
+            <CopyToClipboard text={props.textToCopy}>
+              <button>{confirmCopy || 'Copy'}</button>
+            </CopyToClipboard>
+          ) : (
+            <button onClick={action}>{confirmCopy || 'Yes'}</button>
+          )}
           <button onClick={cancel}>{cancelCopy || 'Cancel'}</button>
         </div>
       </div>
