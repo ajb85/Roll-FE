8743cef5c8f0c0f0eb925d57bac6842739df7934
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-26 14:21:36 -0500

    Expands default color styles to p and svg tags inside of buttons

--- src/Styles.js
+++ src/Styles.js
@@ -37,9 +37,27 @@ export const AppContainer = styled.div`
     color: ${props => props.colors.secondary};
     border: 1px solid ${props => props.colors.secondary};
 
+    p {
+      color: ${props => props.colors.secondary};
+      transition: background-color 0.5s, color 0.5s, border-color 0.5s;
+    }
+
+    svg {
+      color: ${props => props.colors.secondary};
+      transition: background-color 0.5s, color 0.5s, border-color 0.5s;
+    }
+
     &:hover {
       background-color: ${props => props.colors.secondary};
       color: ${props => props.colors.primary};
+
+      p {
+        color: ${props => props.colors.primary};
+      }
+
+      svg {
+        color: ${props => props.colors.primary};
+      }
     }
   }
 `;
