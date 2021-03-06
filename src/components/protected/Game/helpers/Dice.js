9816a745fd649da08ad327277345d008613ebc46
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-06 15:52:46 -0500

    Fixes dice image imports

--- src/components/protected/Game/helpers/Dice.js
+++ src/components/protected/Game/helpers/Dice.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import styles from '../styles.module.scss';
+// Roll! dice
+import rDie from "img/R.png";
+import oDie from "img/O.png";
+import lDie from "img/L.png";
+import exclaimDie from "img/e.png";
+
+// Numbered dice
+import oneDie from "img/1.png";
+import twoDie from "img/2.png";
+import threeDie from "img/3.png";
+import fourDie from "img/4.png";
+import fiveDie from "img/5.png";
+import sixDie from "img/6.png";
+
+// Numbered dice (locked)
+import oneDieLocked from "img/1l.png";
+import twoDieLocked from "img/2l.png";
+import threeDieLocked from "img/3l.png";
+import fourDieLocked from "img/4l.png";
+import fiveDieLocked from "img/5l.png";
+import sixDieLocked from "img/6l.png";
+
+import styles from "../styles.module.scss";
+
+const diceImages = [null, oneDie, twoDie, threeDie, fourDie, fiveDie, sixDie];
+const diceImagesLocked = [
+  null,
+  oneDieLocked,
+  twoDieLocked,
+  threeDieLocked,
+  fourDieLocked,
+  fiveDieLocked,
+  sixDieLocked,
+];
 
 function Dice({ localState, toggleLockOnDie }) {
   const { isTurn, locked, game } = localState;
@@ -8,20 +41,25 @@ function Dice({ localState, toggleLockOnDie }) {
     game && game.rolls && game.rolls.length
       ? game.rolls[game.rolls.length - 1]
       : [];
+  const hasDiceRolls = dice && dice.length;
 
   return (
     <section className={styles.dice}>
-      {dice && dice.length ? (
-        dice.map((_, i) => (
-          <img
-            key={i}
-            onClick={() => toggleLockOnDie(i)}
-            src={require(`../../../../img/${dice[i]}${
-              locked[i] ? 'l' : ''
-            }.png`)}
-            alt='die'
-          />
-        ))
+      {hasDiceRolls ? (
+        dice.map((dieFace, i) => {
+          const isLocked = locked[i];
+          const dieImage = isLocked
+            ? diceImagesLocked[dieFace]
+            : diceImages[dieFace];
+          return (
+            <img
+              key={i}
+              onClick={toggleLockOnDie.bind(this, i)}
+              src={dieImage}
+              alt={`${dieFace} die`}
+            />
+          );
+        })
       ) : (
         <RollDice isTurn={isTurn} />
       )}
@@ -34,33 +72,33 @@ function RollDice({ isTurn }) {
     <>
       <img
         style={{ opacity: isTurn ? 1 : 0.5 }}
-        src={require('img/R.png')}
-        key={'R'}
-        alt='R die'
+        src={rDie}
+        key={"R"}
+        alt="R die"
       />
       <img
         style={{ opacity: isTurn ? 1 : 0.5 }}
-        src={require('img/O.png')}
-        key={'O'}
-        alt='O die'
+        src={oDie}
+        key={"O"}
+        alt="O die"
       />
       <img
         style={{ opacity: isTurn ? 1 : 0.5 }}
-        src={require('img/L.png')}
-        key={'L1'}
-        alt='L die'
+        src={lDie}
+        key={"L1"}
+        alt="L die"
       />
       <img
         style={{ opacity: isTurn ? 1 : 0.5 }}
-        src={require('img/L.png')}
-        key={'L2'}
-        alt='L die'
+        src={lDie}
+        key={"L2"}
+        alt="L die"
       />
       <img
         style={{ opacity: isTurn ? 1 : 0.5 }}
-        src={require('img/e.png')}
-        key={'!'}
-        alt='! die'
+        src={exclaimDie}
+        key={"!"}
+        alt="! die"
       />
     </>
   );
