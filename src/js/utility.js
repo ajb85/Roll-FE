43b65c595dd855bc52ed0732083be015e6bab581
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-08 23:35:30 -0500

    Adds function to route user

--- src/js/utility.js
+++ src/js/utility.js
@@ -1,2 +1,12 @@
+import history from "history.js";
+
 export const isObject = (obj) =>
   typeof obj === "object" && !Array.isArray(obj) && obj !== null;
+
+export const routeUser = (route) => {
+  if (typeof route !== "string") {
+    // Event object or bad data, send user back to slash route
+    route = "/";
+  }
+  history.push("/");
+};
