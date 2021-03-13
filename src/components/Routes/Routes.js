dbb1a4c0a641bba286e6ba549b003c8a74913964
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-13 03:19:23 -0500

    inits state fetchers tied to routes

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,49 +1,56 @@
-import React, { useEffect } from 'react';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect } from "react";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import GameSelect from 'components/protected/GameSelect/';
-import NewGame from 'components/protected/GameSelect/NewGame/';
-import JoinGame from 'components/protected/GameSelect/JoinGame/';
-import PlayGame from 'components/protected/Game/';
+import GameSelect from "components/protected/GameSelect/";
+import NewGame from "components/protected/GameSelect/NewGame/";
+import JoinGame from "components/protected/GameSelect/JoinGame/";
+import PlayGame from "components/protected/Game/";
+import FetchActiveGame from "components/Fetchers/ActiveGame.js";
 
-import { getUsersGames } from 'reducers/games.js';
+import { getUsersGames } from "reducers/games.js";
 
 function Routes(props) {
   const { gamesWereFetched, games, getUsersGames } = props;
+
   useEffect(() => {
     if (!gamesWereFetched && !games.length) {
       getUsersGames();
     }
   }, [gamesWereFetched, games, getUsersGames]);
+
   return (
     <React.Fragment>
-      <Route path='/' exact>
+      <Route path="/" exact>
         <GameSelect />
       </Route>
 
-      <Route path='/game/create'>
+      <Route path="/game/create">
         <NewGame />
       </Route>
 
-      <Route path='/game/join'>
+      <Route path="/game/join">
         <JoinGame />
       </Route>
 
-      <Route path='/game/play/:game_id'>
+      <Route path="/game/play/:game_id">
         <PlayGame />
       </Route>
 
-      <Route path='/j/:uuid'>
+      <Route path="/game/play/:game_id">
+        <FetchActiveGame />
+      </Route>
+
+      <Route path="/j/:uuid">
         <JoinGame />
       </Route>
     </React.Fragment>
   );
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   games: state.games.active,
-  gamesWereFetched: state.games.wereFetched
+  gamesWereFetched: state.games.wereFetched,
 });
 
 export default connect(mapStateToProps, { getUsersGames })(Routes);
