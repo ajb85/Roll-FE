47e4f85e664aa212e26511bffd3cebf64b5fd137
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-13 03:23:41 -0500

    Adds a border to active games that aren't the user's turn

--- src/components/protected/GameSelect/Table/Styles.js
+++ src/components/protected/GameSelect/Table/Styles.js
@@ -5,10 +5,11 @@ export const Row = styled.tr`
     props.isUsersTurn && props.isActive
       ? props.colors.secondary
       : props.colors.primary};
+  border: ${({isActive, isUsersTurn, colors}) => isActive && !isUsersTurn ? `1px solid ${colors.secondary}` : '1px solid transparent'};
   color: ${props =>
     props.isUsersTurn && props.isActive
       ? props.colors.primary
       : props.colors.secondary};
 
-  border-radius: ${props => (props.isUsersTurn ? '7px' : 0)};
+  border-radius: ${props => (props.isActive ? '7px' : 0)};
 `;
