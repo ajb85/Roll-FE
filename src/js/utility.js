d25adb4a688af446a5edba89ed344399b9c3dc17
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-11 16:45:03 -0400

    Changes rgba function to be just rgb

--- src/js/utility.js
+++ src/js/utility.js
@@ -54,23 +54,22 @@ export function valueExists(v) {
   return !!v;
 }
 
-export function hexToRGBA(h, a) {
+export function hexToRGB(h) {
   let r = 0,
     g = 0,
     b = 0;
 
-  // 3 digits
   if (h.length === 4) {
+    // 3 digits
     r = "0x" + h[1] + h[1];
     g = "0x" + h[2] + h[2];
     b = "0x" + h[3] + h[3];
-
-    // 6 digits
   } else if (h.length === 7) {
+    // 6 digits
     r = "0x" + h[1] + h[2];
     g = "0x" + h[3] + h[4];
     b = "0x" + h[5] + h[6];
   }
 
-  return "rgb(" + +r + "," + +g + "," + +b + ", " + a + ")";
+  return +r + ", " + +g + ", " + +b;
 }
