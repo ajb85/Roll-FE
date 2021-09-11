864ce6f9c566bb244dc0476d3bdfa458d09c52e8
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-11 17:12:16 -0400

    checks for no data when routing user

--- src/js/utility.js
+++ src/js/utility.js
@@ -3,7 +3,7 @@ import history from "history.js";
 export const isObject = (obj) => typeof obj === "object" && !Array.isArray(obj) && obj !== null;
 
 export const routeUser = (route) => {
-  if (typeof route !== "string") {
+  if (!route || typeof route !== "string") {
     // Event object or bad data, send user back to slash route
     route = "/";
   }
