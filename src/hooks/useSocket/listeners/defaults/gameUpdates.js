08730e34817623a8e57c5c4acf9aeb5fb88b716f
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-10-01 18:57:46 -0400

    corrects log to logs

--- src/hooks/useSocket/listeners/defaults/gameUpdates.js
+++ src/hooks/useSocket/listeners/defaults/gameUpdates.js
@@ -1,5 +1,7 @@
 export default function gameUpdates(game, emit_id) {
-  const game_id = game?.game_id || game?.log?.[0]?.game_id;
+  const isLog = game?.logs;
+  const game_id = isLog ? game.logs[0]?.game_id : game?.game_id;
+
   if (game_id) {
     emit_id && (this.emitLog[game_id] = emit_id);
     this.games.update(game_id, game);
